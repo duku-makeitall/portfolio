70b7b97e5faf402f6813327adcdb68e25a145a6d
duku-makeitall <duku@makeitall.co.kr>
2026-07-14 15:58:47 +0900

    오류 수정

--- api/send-email.js
+++ api/send-email.js
@@ -34,8 +34,6 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Origin": "http://localhost:8080",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
       },
       body: JSON.stringify({
         service_id: serviceId,
